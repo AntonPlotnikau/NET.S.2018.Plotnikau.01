62f7a729b7c84c81f92797ca77602319a62b4cb1
Антон Плотников <7210697@gmail.com>
2018-03-15 22:20:51 +0300

    Update tests

--- SortsTests/MergeSortTests.cs
+++ SortsTests/MergeSortTests.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sorts;
 
-namespace SortsTests
+namespace Sorts.Tests
 {
     /// <summary>
-    /// This class tests QuickSort.
+    /// This class tests MergeSort.
     /// </summary>
     [TestClass]
-    public class QuickSortTest
+    public class MergeSortTests
     {
         /// <summary>
-        /// This method checks the source array after sorting and the expected array.
+        /// This method checks the source array after sorting and the expected array. 
         /// </summary>
         [TestMethod]
         public void Sorting_UnsortedArray_SortedArray()
@@ -18,7 +18,7 @@ namespace SortsTests
             int[] sourceArray = { 3, 2, 4, 1, 5 };
             int[] expectedArray = { 1, 2, 3, 4, 5 };
 
-            QuickSort.Sorting(sourceArray);
+            MergeSort.Sorting(sourceArray);
 
             CollectionAssert.AreEqual(sourceArray, expectedArray);
         }
@@ -30,25 +30,24 @@ namespace SortsTests
         public void Sorting_ZeroCapacity()
         {
             int[] sourceArray = new int[0];
-            int[] expectedArray = new int[0];
+            int[] expectedArray = sourceArray;
 
-            QuickSort.Sorting(sourceArray);
+            MergeSort.Sorting(sourceArray);
 
-            Assert.ReferenceEquals(sourceArray, expectedArray);
+            Assert.AreSame(expectedArray, sourceArray);
         }
 
         /// <summary>
         /// Checks sort for null reference processing.
         /// </summary>
         [TestMethod]
-        public void Sorting_NullReference()
+        public void Sorting_ActualNullReference_ExpectedNullReference()
         {
             int[] sourceArray = null;
-            int[] expectedArray = null;
 
-            QuickSort.Sorting(sourceArray);
+            MergeSort.Sorting(sourceArray);
 
-            Assert.ReferenceEquals(sourceArray, expectedArray);
+            Assert.IsNull(sourceArray);
         }
     }
 }
--- SortsTests/QuickSortTests.cs
+++ SortsTests/QuickSortTests.cs
@@ -1,16 +1,16 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Sorts;
 
-namespace SortsTests
+namespace Sorts.Tests
 {
     /// <summary>
-    /// This class tests MergeSort.
+    /// This class tests QuickSort.
     /// </summary>
     [TestClass]
-    public class MergeSortTest
+    public class QuickSortTests
     {
         /// <summary>
-        /// This method checks the source array after sorting and the expected array. 
+        /// This method checks the source array after sorting and the expected array.
         /// </summary>
         [TestMethod]
         public void Sorting_UnsortedArray_SortedArray()
@@ -18,7 +18,7 @@ namespace SortsTests
             int[] sourceArray = { 3, 2, 4, 1, 5 };
             int[] expectedArray = { 1, 2, 3, 4, 5 };
 
-            MergeSort.Sorting(sourceArray);
+            QuickSort.Sorting(sourceArray);
 
             CollectionAssert.AreEqual(sourceArray, expectedArray);
         }
@@ -27,28 +27,27 @@ namespace SortsTests
         /// Checks sort for zero capacity processing.
         /// </summary>
         [TestMethod]
-        public void Sorting_ActualZeroCapacity_ExpectedZeroCapacity()
+        public void Sorting_ZeroCapacity()
         {
             int[] sourceArray = new int[0];
-            int[] expectedArray = new int[0];
+            int[] expectedArray = sourceArray;
 
-            MergeSort.Sorting(sourceArray);
+            QuickSort.Sorting(sourceArray);
 
-            Assert.ReferenceEquals(sourceArray, expectedArray);
+            Assert.AreSame(expectedArray, sourceArray);
         }
 
         /// <summary>
         /// Checks sort for null reference processing.
         /// </summary>
         [TestMethod]
-        public void Sorting_ActualNullReference_ExpectedNullReference()
+        public void SortingNullTest()
         {
             int[] sourceArray = null;
-            int[] expectedArray = null;
 
-            MergeSort.Sorting(sourceArray);
+            QuickSort.Sorting(sourceArray);
 
-            Assert.ReferenceEquals(sourceArray, expectedArray);
+            Assert.IsNull(sourceArray);
         }
     }
 }
